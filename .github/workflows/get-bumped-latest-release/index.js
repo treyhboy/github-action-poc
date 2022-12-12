01b3eb3c62c25932fcd26f5a8afae53f910eacd6
Tarun Trehan <taruntrehen@gmail.com>
2022-12-12 11:41:38 +0530

    fixes

---
+++ .github/workflows/get-bumped-latest-release/index.js
@@ -0,0 +1,46 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+async function run() {
+    const version = await getLatestPreRelease()
+    core.setOutput("incremented_version", incrementSemanticVersion(version));
+}
+
+async function getLatestPreRelease() {
+    try {
+        const token = core.getInput('token');
+        const octokit = github.getOctokit(token);
+        var releases  = await octokit.repos.listReleases({
+            repo: github.context.repo.repo,
+            owner: github.context.repo.owner,
+            per_page: 100,
+            page:1,
+            });
+        releases = releases.data;
+        releases = releases.filter(x => x.prerelease == true);
+        if (releases.length) {
+            return releases[0].tag_name
+        } else {
+            core.setFailed("No valid releases");
+            return ""
+        }
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+function incrementSemanticVersion(version) {
+   const [major, minor, patch,rc] = version.split('.');
+   return major +"."+ minor +"."+ patch +"."+ (Number(rc)+1)
+}
+
+if (!module.parent) {
+  try {
+    run()
+  } catch (error) {
+    core.setFailed(error.message)
+    throw error
+  }
+}
